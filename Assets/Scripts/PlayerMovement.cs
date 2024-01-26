d47d9ca0bf589696104c31058e5f5d86c3579508
TuomoO <tuomo.oksanen@zaibatsu.fi>
2024-01-26 23:46:57 +0200

    Move left and right

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private const float Speed = 10;
+
+    private bool _moving;
+    private Vector2 _input;
+
+    private void Update()
+    {
+        if (!_moving)
+        {
+            return;
+        }
+
+        transform.position += new Vector3(_input.x, 0, 0) * (Time.deltaTime * Speed);
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("Move!");
+        _moving = context.control.IsPressed();
+        _input = context.ReadValue<Vector2>();
     }
-    
+
     public void Join(InputAction.CallbackContext context)
     {
         Debug.Log("Join!");
