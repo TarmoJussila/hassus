22c9b43cb95adf1edef100a3d7de470887261f6a
Aleksi Toivonen <aleksi.toivonen@zaibatsu.fi>
2024-01-28 00:25:26 +0200

    he dead

--- Assets/Creampie.cs
+++ Assets/Creampie.cs
@@ -8,12 +8,12 @@ public class Creampie : SpawnedWeaponBase
     [SerializeField] private int _damage;
     [SerializeField] private GameObject _killParticle;
 
-    protected override void OnHitEnemy(Collider2D other)
+    protected override void OnCollideEnemy(Collision2D coll)
     {
-        PlayerHealth health = other.GetComponent<PlayerHealth>();
+        PlayerHealth health = coll.gameObject.GetComponent<PlayerHealth>();
         health.TakeDamage(_damage, OwnerPlayer.playerIndex);
 
-        if (health.CurrentHealth <= 0)
+        if (health.CurrentHealth <= 0 && _killParticle != null)
         {
             Instantiate(_killParticle, health.transform.position, Quaternion.identity);
         }
