fa89612d109f816321ddeb9009cc32976d216f3b
Aleksi Toivonen <aleksi.toivonen@zaibatsu.fi>
2024-01-28 03:33:59 +0200

    improved sounds

--- Assets/Scripts/Weapons/Banana.cs
+++ Assets/Scripts/Weapons/Banana.cs
@@ -8,12 +8,18 @@ public class Banana : SpawnedWeaponBase
     [SerializeField] private int _damage;
     [SerializeField] private GameObject _killParticle;
 
+    protected override void OnStart()
+    {
+        SFXManager.Instance.PlayOneShot(SFXType.Wobble);
+    }
+
     protected override void OnCollideEnemy(Collision2D coll)
     {
         PlayerHealth health = coll.gameObject.GetComponent<PlayerHealth>();
         health.TakeDamage(_damage, OwnerPlayer.playerIndex);
 
         Destroy(gameObject);
+        SFXManager.Instance.PlayOneShot(SFXType.Vineboom);
         SFXManager.Instance.PlayOneShot(SFXType.Wobble);
     }
 
@@ -23,13 +29,13 @@ public class Banana : SpawnedWeaponBase
         health.TakeDamage(_damage, OwnerPlayer.playerIndex);
 
         Destroy(gameObject);
+        SFXManager.Instance.PlayOneShot(SFXType.Vineboom);
         SFXManager.Instance.PlayOneShot(SFXType.Wobble);
     }
 
     private void OnDestroy()
     {
         StopAllCoroutines();
-        GetComponent<AudioSource>().Play();
 
         if (_killParticle != null)
         {
