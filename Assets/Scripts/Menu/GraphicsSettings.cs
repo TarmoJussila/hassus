b0b4ce56a8d4a23607414e2eb71597bfaa9e14b6
TuomoO <tuomo.oksanen@zaibatsu.fi>
2024-01-28 02:06:53 +0200

    Apply button

--- Assets/Scripts/Menu/GraphicsSettings.cs
+++ Assets/Scripts/Menu/GraphicsSettings.cs
@@ -1,32 +1,52 @@
 using TMPro;
 using UnityEngine;
 
-public class GraphicsSettings : MonoBehaviour
+namespace Hassus.Menu
 {
-    [SerializeField] private TMP_Dropdown resolutionDropdown;
-
-    private Resolution[] _resolutions;
-    private int _selectedIndex = -1;
-
-    private void Start()
+    public class GraphicsSettings : MonoBehaviour
     {
-        _resolutions = Screen.resolutions;
-        resolutionDropdown.ClearOptions();
+        [SerializeField] private TMP_Dropdown resolutionDropdown;
 
-        foreach (Resolution resolution in _resolutions)
+        private Resolution[] _resolutions;
+        private int _selectedIndex = -1;
+
+        public bool FullScreenEnabled { get; set; }
+
+        private void Start()
         {
-            float rate = resolution.refreshRateRatio.numerator / (float)resolution.refreshRateRatio.denominator;
-            string text = $"{resolution.width} x {resolution.height} {rate:0.00} Hz";
-            resolutionDropdown.options.Add(new TMP_Dropdown.OptionData
-            {
-                text = text
-            });
-        }
-    }
+            FullScreenEnabled = Screen.fullScreen;
+            _resolutions = Screen.resolutions;
+            resolutionDropdown.ClearOptions();
 
-    public void SelectResolution(int index)
-    {
-        _selectedIndex = index;
-        Debug.Log($"Selected: {resolutionDropdown.options[index].text}");
+            foreach (Resolution resolution in _resolutions)
+            {
+                float rate = resolution.refreshRateRatio.numerator / (float)resolution.refreshRateRatio.denominator;
+                string text = $"{resolution.width} x {resolution.height} {rate:0.00} Hz";
+                resolutionDropdown.options.Add(new TMP_Dropdown.OptionData
+                {
+                    text = text
+                });
+            }
+        }
+
+        public void SelectResolution(int index)
+        {
+            _selectedIndex = index;
+            Debug.Log($"Selected: {resolutionDropdown.options[index].text}");
+        }
+
+        public void OnApplyClick()
+        {
+            if (_selectedIndex < 0 || _resolutions == null)
+            {
+                Debug.Log("Skipped options menu apply, no resolution selected");
+                return;
+            }
+
+            Screen.fullScreen = FullScreenEnabled;
+            Resolution res = _resolutions[_selectedIndex];
+            Screen.SetResolution(res.width, res.height,
+                FullScreenEnabled ? FullScreenMode.ExclusiveFullScreen : FullScreenMode.Windowed, res.refreshRateRatio);
+        }
     }
 }
